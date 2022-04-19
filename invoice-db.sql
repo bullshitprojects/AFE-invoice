55c153d66873a2c19e565132250b7ae189ce2a59
Elisa <elisa.pereira2000@gmail.com>
2022-04-18 20:05:58 -0600

    db script fixed

--- invoice-db.sql
+++ invoice-db.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE afeinvoice;
-USE afeincoive;
+USE afeinvoice;
 
 CREATE TABLE product (
 id_product INT PRIMARY KEY auto_increment,
 code VARCHAR(8) UNIQUE NOT NULL,
 name VARCHAR (50) NOT NULL,
+description VARCHAR(150) NOT NULL,
 quantity INT NOT NULL,
 unity_price DOUBLE NOT NULL
 );
